46932ef34e2cf31e4784458c41e8465bd4a6c260
pan032 <68030276@kmitl.ac.th>
2025-06-17 07:39:17 +0000

    jjjj

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,4 +1,4 @@
-um1 = int(input("Enter the first number: "))
+num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 result = num1 * num2
 print(f"{num1} x {num2} = {result}")
