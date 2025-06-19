92b2edcf7dd214284df83ea5e13e1288d8f9b302
pan032 <68030276@kmitl.ac.th>
2025-06-19 03:28:26 +0000

    er

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,6 @@
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array =  []
+for num in original_array:
+    new_array.append(num + 2)
+print("Original_array:", original_array)
+print("New array (each value +2):", new_array)
