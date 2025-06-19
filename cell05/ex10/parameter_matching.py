167eca464929cc500202d86f6dfdb13dbb507bce
pan032 <68030276@kmitl.ac.th>
2025-06-19 04:02:35 +0000

    jkky

---
+++ cell05/ex10/parameter_matching.py
@@ -0,0 +1,9 @@
+import sys
+if len(sys.argv) != 2:
+    print("none")
+else:
+    user_input = input("Enter a word: ")
+    if user_input == sys.argv[1]:
+        print("Good job!")
+    else:
+        print("Nope, sorry...")
