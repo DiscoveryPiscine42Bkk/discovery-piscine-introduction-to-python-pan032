a0c344ef6439599548af2c69093bf2bd8a6094ec
pan032 <68030276@kmitl.ac.th>
2025-06-19 04:08:35 +0000

    hyh

---
+++ cell05/ex11/count_it.py
@@ -0,0 +1,8 @@
+import sys
+if len(sys.argv) == 1:
+    print("none")
+else:
+    param_count = len(sys.argv) - 1
+    print(f"parameters: {param_count}")
+    for param in sys.argv[1:]:
+        print(f"{param} {len(param)}")
